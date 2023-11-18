d6f2c045a37b941210f2fb4d3466f3564ce03d90
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-11-18 13:38:09 +0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using static Obeliskial_Essentials.Essentials;
+
+namespace TraitMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.stiffmeds.obeliskialessentials")]
+    [BepInProcess("AcrossTheObelisk.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal const int ModDate = 20231118;
+        private readonly Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            Log = Logger;
+            Log.LogInfo($"{PluginInfo.PLUGIN_GUID} {PluginInfo.PLUGIN_VERSION} has loaded!");
+            // register with Obeliskial Essentials
+            AddModVersionText(PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION, ModDate.ToString());
+            // apply patches
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Traits.cs
@@ -0,0 +1,62 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Obeliskial_Content;
+
+namespace TraitMod
+{
+    [HarmonyPatch]
+    internal class Traits
+    {
+        public static string[] myTraitList = { "exampletrait" };
+
+        public static void myDoTrait(string _trait, ref Trait __instance)
+        {
+            // get info you may need
+            Enums.EventActivation _theEvent = Traverse.Create(__instance).Field("theEvent").GetValue<Enums.EventActivation>();
+            Character _character = Traverse.Create(__instance).Field("character").GetValue<Character>();
+            Character _target = Traverse.Create(__instance).Field("target").GetValue<Character>();
+            int _auxInt = Traverse.Create(__instance).Field("auxInt").GetValue<int>();
+            string _auxString = Traverse.Create(__instance).Field("auxString").GetValue<string>();
+            CardData _castedCard = Traverse.Create(__instance).Field("castedCard").GetValue<CardData>();
+            Traverse.Create(__instance).Field("character").SetValue(_character);
+            Traverse.Create(__instance).Field("target").SetValue(_target);
+            Traverse.Create(__instance).Field("theEvent").SetValue(_theEvent);
+            Traverse.Create(__instance).Field("auxInt").SetValue(_auxInt);
+            Traverse.Create(__instance).Field("auxString").SetValue(_auxString);
+            Traverse.Create(__instance).Field("castedCard").SetValue(_castedCard);
+            TraitData traitData = Globals.Instance.GetTraitData(_trait);
+            List<CardData> cardDataList = new List<CardData>();
+            List<string> heroHand = MatchManager.Instance.GetHeroHand(_character.HeroIndex);
+            Hero[] teamHero = MatchManager.Instance.GetTeamHero();
+            NPC[] teamNpc = MatchManager.Instance.GetTeamNPC();
+
+            // activate traits
+            if (_trait == "exampletrait")
+            {
+                // do some things
+            }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Trait), "DoTrait")]
+        public static bool DoTrait(Enums.EventActivation _theEvent, string _trait, Character _character, Character _target, int _auxInt, string _auxString, CardData _castedCard, ref Trait __instance)
+        {
+            if ((UnityEngine.Object)MatchManager.Instance == (UnityEngine.Object)null)
+                return false;
+            Traverse.Create(__instance).Field("character").SetValue(_character);
+            Traverse.Create(__instance).Field("target").SetValue(_target);
+            Traverse.Create(__instance).Field("theEvent").SetValue(_theEvent);
+            Traverse.Create(__instance).Field("auxInt").SetValue(_auxInt);
+            Traverse.Create(__instance).Field("auxString").SetValue(_auxString);
+            Traverse.Create(__instance).Field("castedCard").SetValue(_castedCard);
+            if (Content.medsCustomTraitsSource.Contains(_trait) && myTraitList.Contains(_trait))
+            {
+                myDoTrait(_trait, ref __instance);
+                return false;
+            }
+            return true;
+        }
+    }
+}
